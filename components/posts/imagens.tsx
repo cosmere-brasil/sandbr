3f91814dc3f369393ed7f908263a28c4d17c4d05
raave-aires <cosmere-brasil@proton.me>
2024-11-21 14:39:37 -0300

    remove: dialog de imagem

--- components/posts/imagens.tsx
+++ components/posts/imagens.tsx
@@ -1,16 +1,15 @@
 "use client"
 
 // importação de dependências:
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 // importação de componentes:
 import { Card, CardContent } from "@/components/ui/card"
-import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button";
 import { Download } from "lucide-react";
 
 interface Dados_das_Imagens {
-  fonte: string,
+  fonte: string | StaticImageData,
   alt: string,
   link_do_autor: string,
   autor: string,
@@ -18,7 +17,7 @@ interface Dados_das_Imagens {
 }
 
 const Imagem: React.FC<Dados_das_Imagens> = ({fonte, alt, link_do_autor, autor, descricao}) => {
-  const url: string = fonte;
+  const url: string | StaticImageData = fonte;
   
   const baixar_imagem = async () => {
     try {
@@ -40,27 +39,20 @@ const Imagem: React.FC<Dados_das_Imagens> = ({fonte, alt, link_do_autor, autor, 
   };
 
   const BotaoBaixar = () => {
-    return <Button onClick={baixar_imagem} className="p-0 bg-transparent text-white hover:bg-transparent hover:text-destaque"><Download/></Button> 
+    return <Button onClick={baixar_imagem} className="p-0 bg-transparent text-white hover:bg-transparent hover:text-destaque self-end"><Download/></Button> 
   }
 
   return (
-    <Dialog>
-      <div>
-        <Card>
-          <CardContent className="p-3 flex flex-col gap-2 ">
-            <DialogTrigger><Image src={fonte} alt={alt} width={350} height={350} className="rounded-md"/></DialogTrigger>
-            <p className="text-xs">Feita por <a href={link_do_autor} target="_blank" className="text-destaque">{autor}.</a></p>
-            <p className="text-sm">{descricao}</p>
-          </CardContent>
-        </Card>
-      </div>
-      
-
-      <DialogContent className="max-w-[720px] backdrop-blur-sm">
-        <DialogTitle className="flex items-center gap-3 text-sm">{descricao} <BotaoBaixar/></DialogTitle>
-        <Image src={fonte} alt={alt} width={720} height={720} className="rounded-md"/>
-      </DialogContent>
-    </Dialog>
+    <div>
+      <Card>
+        <CardContent className="p-3 flex flex-col gap-2 ">
+          <Image src={fonte} alt={alt} width={350} height={350} className="rounded-md"/>
+          <p className="text-xs">Feita por <a href={link_do_autor} target="_blank" className="text-destaque">{autor}.</a></p>
+          <p className="text-sm">{descricao}</p>
+          <BotaoBaixar/>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
