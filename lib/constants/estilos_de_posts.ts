7b2926971a49f64115224902af87ff44d75393c9
raave-aires <cosmere-brasil@proton.me>
2024-11-21 15:00:25 -0300

    delete: etilos_de_posts

--- lib/constants/estilos_de_posts.ts
+++
@@ -1,9 +0,0 @@
-const estilos = {
-    caminho: "sticky top-14 h-8 bg-background flex items-center z-50",
-    main: "w-[80%] flex flex-col gap-3",
-    article: "flex",
-    post: "w-[70%] px-5",
-    complementar: "w-[30%] flex flex-col items-center"
-}
-
-export { estilos }
