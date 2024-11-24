baadf46c0caaf625cc0061719fc2461ddb095812
raave-aires <cosmere-brasil@proton.me>
2024-11-24 20:36:57 -0300

    style: const de estilos atualizada

--- app/sobre/licencas/page.tsx
+++ app/sobre/licencas/page.tsx
@@ -19,10 +19,10 @@ export default function Pagina() {
   const liint_props = "pl-6"
   const estilos = {
     caminho: "sticky top-14 h-8 bg-background flex items-center z-40",
-    main: "w-[80%] flex flex-col gap-3",
+    main: "w-screen lg:w-[80%] flex flex-col gap-3",
     article: "flex",
-    post: "w-[70%] px-5",
-    complementar: "w-[30%] flex flex-col items-center rounded-sm"
+    post: "w-screen lg:w-[70%] px-5",
+    complementar: "hidden lg:block w-[30%] flex flex-col items-center rounded-sm"
   }
 
   const ano = new Date().getFullYear().toString();
