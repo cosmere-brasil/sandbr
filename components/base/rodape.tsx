892b438cab37d1698ee45a25379836a0b3d3fa6d
raave-aires <cosmere-brasil@proton.me>
2024-11-25 01:14:24 -0300

    rename: de footer para rodape

--- components/base/rodape.tsx
+++ components/base/rodape.tsx
@@ -8,18 +8,19 @@ interface Enlaces {
 
 const Enlaçar: React.FC<Enlaces> = ({ destino, texto }) => {
   return (
-      <Link href={destino} className="hover:underline me-4 md:me-6" prefetch={true}>
+      <Link href={destino} className="hover:underline" prefetch={true}>
         {texto}
       </Link>
   );
 };
+
 export function Rodape() {
   const ano = new Date().getFullYear().toString().slice(1)
   return (
     <footer className="bg-background border-t">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 12.{ano} EH, <Link href="/" className="hover:underline">Sandbr</Link>. Distribuído sob as licenças <Ir destino="https://github.com/sand-br/sandbr/blob/main/LICENSE.md" texto="GPL v3"/> e <Ir destino="https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.pt" texto="CC BY-NC-SA 4.0"/>.</span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <span className="text-sm text-gray-400 sm:text-center">© <Enlaçar destino="/sobre/calendario-holoceno" texto={`12.${ano}`}/> EH, <Link href="/" className="hover:underline">Sandbr</Link>. Distribuído sob as licenças <Ir destino="https://github.com/sand-br/sandbr/blob/main/LICENSE.md" texto="GPL v3"/> e <Ir destino="https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.pt" texto="CC BY-NC-SA 4.0"/>.</span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 gap-3">
           <li>
             <Enlaçar destino="/sobre" texto="Sobre a Sandbr"/>
           </li>
